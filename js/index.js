3b2f3b82d9619e4ffc9734282622e48110389ccf
mmolta <mcmolta10@gmail.com>
2021-01-07 16:42:39 -0500

    set up scenes.js and controller in index

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
-// load scroll magic controller
-// import "scenes" and attach 
+// import "scenes"
+import { whatIsLTSScene, regionalLTSScene } from './scenes.js'
+
+// init controller
+const controller = new ScrollMagic.Controller();
+
+// add scenes to controller
+whatIsLTSScene.addTo(controller)
+regionalLTSScene.addTo(controller)
--- js/scenes.js
+++ js/scenes.js
@@ -1,1 +1,16 @@
-// one fnc per "scene" export all to index and attach to the main controller
+// get scenes
+const scenes = document.querySelectorAll('.scene-section')
+
+// create scenes (order matters)
+const whatIsLTSScene = new ScrollMagic.Scene({
+    duration: 100,
+    triggerElement: scenes[0],
+    reverse: true
+})
+const regionalLTSScene = new ScrollMagic.Scene({
+    duration: 100,
+    triggerElement: scenes[1],
+    reverse: true
+}).setPin('#scrollstory-map') // pins the map for the the scene's duration
+
+export { whatIsLTSScene, regionalLTSScene }
