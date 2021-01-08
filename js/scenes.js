b1bd402379f15619836994592f7a3b6c684454b2
mmolta <mcmolta10@gmail.com>
2021-01-08 15:54:09 -0500

    add animation hook to scenes + begin scroll nav

--- js/scenes.js
+++ js/scenes.js
@@ -7,10 +7,19 @@ const whatIsLTSScene = new ScrollMagic.Scene({
     triggerElement: scenes[0],
     reverse: true
 })
+.on('enter', e => {
+    console.log('entered whatisLTS scene')
+    // toggle animation here to reveal element 
+})
 const regionalLTSScene = new ScrollMagic.Scene({
     duration: 100,
     triggerElement: scenes[1],
     reverse: true
-}).setPin('#scrollstory-map') // pins the map for the the scene's duration
+})
+.on('enter', e => {
+    console.log('entered regional LTS scene')
+    // toggle animation here to reveal element 
+})
+.setPin('#scrollstory-map') // pins the map for the the scene's duration
 
 export { whatIsLTSScene, regionalLTSScene }
