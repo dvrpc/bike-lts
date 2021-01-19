462badc019c6dfd74d31a7a4e221d58954c5c361
mmolta <mcmolta10@gmail.com>
2021-01-19 17:23:32 -0500

    add 2nd map + delete old placeholder imgs

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ import sceneObjs from './scenes.js'
 import { customStyles } from './map.js'
 
 const mapContainer = document.getElementById('map')
+const mapTwoContainer = document.getElementById('map-2')
 
 //***** handle scroll story
 // init controller
@@ -13,15 +14,25 @@ sceneObjs.forEach(scene => scene.addTo(controller))
 
 
 
-//***** handle map
+//***** handle maps
+const containers = [mapContainer, mapTwoContainer]
 mapboxgl.accessToken = 'pk.eyJ1IjoibW1vbHRhIiwiYSI6ImNqZDBkMDZhYjJ6YzczNHJ4cno5eTcydnMifQ.RJNJ7s7hBfrJITOBZBdcOA'
-const map = new mapboxgl.Map({
-    container: mapContainer,
-    style: customStyles,
-    attributionsControl: false,
-    center: [-75.2273, 40.071],
-    zoom: 3,
-    interactive: false
+containers.forEach(container => {
+    const map = new mapboxgl.Map({
+        container: container,
+        style: customStyles,
+        attributionsControl: false,
+        center: [-75.2273, 40.071],
+        zoom: 3,
+        interactive: false
+    })
+
+    map.fitBounds([[-76.09405517578125, 39.49211914385648],[-74.32525634765625,40.614734298694216]]);
 })
-map.fitBounds([[-76.09405517578125, 39.49211914385648],[-74.32525634765625,40.614734298694216]]);
 //***** END map
+
+// @TODO multiple maps
+    // map 1: regional + low-stress islands
+    // map 2: connectivity analysis + connectivity priorities
+    // map 3: special designations
+// alternate alignment i.e. map 1 & 3 have map left, text right while 2 has map right, text left
