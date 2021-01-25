b346c177c040c1d9ee3064b6f17d7d9be5d7c149
mmolta <mcmolta10@gmail.com>
2021-01-25 11:52:23 -0500

    create story nav active class and apply on scene enter

--- js/scenes.js
+++ js/scenes.js
@@ -3,28 +3,44 @@ const scenes = document.querySelectorAll('.scene-section')
 const l = scenes.length
 const sceneObjs = []
 
+// get nav els minus hr
+const scrollNav = document.getElementById('scroll-story-nav-ul').children
+const scrollNavBtns = Array.from(scrollNav).filter(el => el.nodeName != 'HR')
+
 // create scene objects
 for(let i = 0; i < l; i++) {
     sceneObjs.push( new ScrollMagic.Scene({
         duration: 200,
         triggerElement: scenes[i],
-        reverse: false
+        reverse: true
     })
     .on('enter', e => {
         toggleAnimation(e, scenes[i])
-        toggleNavLink(e, scenes[i])
+        toggleNavLink(i)
         toggleMapView(e)
     }))
 }
 
 // helper fncs to animate scene in/out, update side-nav link state and update map (when applicable)
-// el scene format: <section><article><figure></section>
 const toggleAnimation = (e, el) => {
     const text = el.children[1] ? el.children[1] : el.children[0]
     text.classList.add('fadein')
 }
-const toggleNavLink = (e, el) => {
+const toggleNavLink = i => {
+    // remove active class from all links
+    scrollNavBtns.forEach(btn => btn.classList.remove('scroll-story-nav-link-active'))
 
+    // add active class to current scene
+    scrollNavBtns[i].classList.add('scroll-story-nav-link-active')
+}
+const makeNavTooltip = el => {
+    const idToHeader =el.id.split('-').join(' ')
+
+    return `
+        <span>
+            ${idToHeader}
+        </span>
+    `
 }
 // @TODO escape no-map cases
 const toggleMapView = e => {
