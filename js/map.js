bc1228ac8406ba12abbbacafd4a480de072b0da8
mmolta <mcmolta10@gmail.com>
2021-03-09 18:17:56 -0500

    update map src

--- js/map.js
+++ js/map.js
@@ -19,22 +19,17 @@ const customStyles = {
 }
 
 const customMap = container => {
-    const zoom = container.zoom || 8.1
     const center = container.center || [-75.2273, 40.071]
     const bounds = container.bounds || [[-76.09405517578125, 39.49211914385648],[-74.32525634765625,40.614734298694216]]
-
-    console.log('initial zoom ', zoom)
 
     const map = new mapboxgl.Map({
         container: container,
         style: customStyles,
         attributionsControl: false,
         center: center,
-        zoom: zoom,
+        bounds: bounds,
         interactive: false
     })
-
-    map.fitBounds(bounds);
 
     return map
 }
