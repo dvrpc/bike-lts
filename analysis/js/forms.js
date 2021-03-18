625793c42396200111b9dcee9b2b4771af641166
mmolta <mcmolta10@gmail.com>
2021-03-18 13:27:01 -0400

    add layer toggle forms + begin setup for secondary layers

--- analysis/js/forms.js
+++ analysis/js/forms.js
@@ -6,6 +6,9 @@ const processForm = form => {
 // return layer to be toggled
 const toggleLayers = (form, map) => {
     form.onchange = e => {
+        // @TODO: once a list of secondary/tertiary/etc layers is established, do a first-pass creation on the fly so that the default map loads as lightweight as possible
+            // map secondaryMapLayers.js and export those objects into here so they can be appended to the map when needed
+
         const layer = e.target.value
         const visibility = e.target.checked ? 'visible' : 'none'
         
--- analysis/js/index.js
+++ analysis/js/index.js
@@ -2,15 +2,13 @@ import makeMap from './map.js'
 import sources from './mapSources.js'
 import layers from './mapLayers.js'
 import handleModal from './modal.js'
-// add additional imports here (popups, forms, etc)
-
+import { toggleLayers } from './forms.js'
 
 const modal = document.getElementById('modal')
 const modalToggle = document.getElementById('modal-toggle')
 const closeModal = document.getElementById('close-modal')
-// get additional elements here (forms, etc)
-
-
+const forms = Array.from(document.querySelectorAll('.sidebar-form-toggle'))
+console.log(forms)
 // map
 const map = makeMap()
 
@@ -18,7 +16,7 @@ map.on('load', () => {
     for(const source in sources) map.addSource(source, sources[source])
     for(const layer in layers) map.addLayer(layers[layer])
 
-    // add map events here (click, mousemove, etc)
+    forms.forEach(form => toggleLayers(form, map))
 })
 
 
