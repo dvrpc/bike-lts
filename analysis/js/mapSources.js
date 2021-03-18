f450986d3ec161c40148486d41e2a4e6de9f3649
mmolta <mcmolta10@gmail.com>
2021-03-18 13:04:51 -0400

    add lts and lowstress islands layers

--- analysis/js/mapSources.js
+++ analysis/js/mapSources.js
@@ -2,6 +2,10 @@ const sources = {
     'boundaries': {
         type: 'vector',
         url: 'https://tiles.dvrpc.org/data/dvrpc-municipal.json'
+    },
+    'lts': {
+        type: 'vector',
+        url: 'https://tiles.dvrpc.org/data/lts.json'
     }
     // add more sources here
 }
