f8cc1c1e99be238279a7280f51dee27c6df3b738
mmolta <mcmolta10@gmail.com>
2021-03-19 18:44:14 -0400

    update theming

--- analysis/js/mapLayers.js
+++ analysis/js/mapLayers.js
@@ -24,33 +24,33 @@ const layers = {
             'line-color': '#383838'
         }
     },
-    // lowstressIslands: {
-    //     id: "lowstress-islands",
-    //     type: "line",
-    //     source: "lts",
-    //     "source-layer": "lowstress_islands",
-    //     "paint": {
-    //         "line-width": 0.25,
-    //         "line-color": "#498434"
-    //     }
-    // },
-    // existingConditions: {
-    //     id: "existing-conditions",
-    //     type: "line",
-    //     source: "lts",
-    //     "source-layer": "existing_conditions_lts",
-    //     "paint": {
-    //         "line-width": 0.5,
-    //         "line-color": [
-    //             'match', ['get', 'lts'],
-    //             "LTS 1", '#498434',
-    //             "LTS 2", '#72bc58',
-    //             "LTS 3", '#fcd842',
-    //             "LTS 4", '#a50a0a',
-    //             '#fff'
-    //         ]
-    //     }
-    // }
+    lowstressIslands: {
+        id: "lowstress-islands",
+        type: "line",
+        source: "lts",
+        "source-layer": "lowstress_islands",
+        "paint": {
+            "line-width": 0.25,
+            "line-color": "#498434"
+        }
+    },
+    existingConditions: {
+        id: "existing-conditions",
+        type: "line",
+        source: "lts",
+        "source-layer": "existing_conditions_lts",
+        "paint": {
+            "line-width": 0.5,
+            "line-color": [
+                'match', ['get', 'lts'],
+                "LTS 1", '#498434',
+                "LTS 2", '#72bc58',
+                "LTS 3", '#fcd842',
+                "LTS 4", '#a50a0a',
+                '#fff'
+            ]
+        }
+    }
     // add more layers here
 }
 
