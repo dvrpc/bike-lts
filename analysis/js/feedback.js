66f25c223cbbf29e579565c86d7c0f97cb0e4fd0
mmolta <mcmolta10@gmail.com>
2021-03-31 13:36:22 -0400

    finish initial set up for feedback

--- analysis/js/feedback.js
+++ analysis/js/feedback.js
@@ -1,6 +1,8 @@
 // Functions to render feedback form 
 import submitFeedback from './api.js'
 
+// @NOTE: rather than replacing sidebar content with feedback form, superimpose form over sidebar content.
+// this makes the transition back to main sidebar content easier and especially helps with maintaining state
 const createFeedbackForm = () => {
     const form = document.createElement('form')
 
