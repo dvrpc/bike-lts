0aa5c4d6492c7f1d5d43fdb6dc7c91f82eae243b
mmolta <mcmolta10@gmail.com>
2021-04-14 19:19:03 -0400

    finish combined form fncs + add lowstress island steps

--- analysis/js/forms.js
+++ analysis/js/forms.js
@@ -17,21 +17,6 @@ const ltsFilters = {
     ]
 }
 
-// @NEW form handlers
-/*
-    DELETE sidebar-form-toggle and sidebar-form-filter classes. 
-    KEEP sidebar-form
-    give each layer data-layer-type with value "toggle" or "filter"
-    Create larger fnc handleForms
-        create filter array
-        loop thru all checked toggles on change
-        check data-layer-type
-            invoke toggleLayers() for each "toggle" type
-            concat filter for each "filter" type
-            after the loop, invoke filterLayers() with the filter as a param
-                include logic for empty filter i.e. if(filter.length) ['<', 'lts_score', 0] + setFilter ELSE logic
-        spinner logic stays the same
-*/
 const handleForms = (form, map) => {
     form.onchange = e => {
         const spinner = map['_container'].querySelector('.lds-ring')
@@ -56,13 +41,15 @@ const toggleLayers = (toggle, map) => {
     map.setLayoutProperty(layer, 'visibility', visibility)
 }
 
-const filterLayers = (form, toggle, map) => {        
-        // handle special toggle case (currently this is all filters but build it for the future)
+const filterLayers = (form, toggle, map) => { 
+        const layer = toggle.name       
+        
+        // handle meta toggles (currently all filter layers are part of a meta toggle but build it for the future)
         if(toggle.classList.contains('core-lts')) {
             const coreInputs = form.querySelectorAll('.core-lts')
-            const selectedState = {id: toggle.id, state: toggle.checked}
+            const selectedInput = {value: toggle.value, state: toggle.checked}
 
-            handleCoreLayers(coreInputs, selectedState)
+            handleCoreLayers(coreInputs, selectedInput)
         }
 
         // get all checked boxes after handling core layers
@@ -71,7 +58,7 @@ const filterLayers = (form, toggle, map) => {
 
         // loop checked inputs & append each to filter obj
         allChecked.forEach(input => {
-            const layerFilter = ltsFilters[input.id]
+            const layerFilter = ltsFilters[input.value]
             baseFilter = layerFilter ? baseFilter.concat(layerFilter) : baseFilter
         })
 
@@ -79,26 +66,18 @@ const filterLayers = (form, toggle, map) => {
 }
 
 // handle UI changes associated with toggling the core layers
-// @NEW: Low Stress islands toggle is independent on LTS 1 and 2 
-    // it is 1 and 2 but with islands grouped by color
-        // create it as a separate layer b/c it needs data driven styles
-    // remove it from the core layers fnc - the only interaction we need here is the existing conditions jawn
-    // IF LTS 1 or 2 are turned on while islands are on, kill islands. something like that.
 const handleCoreLayers = (coreInputs, selectedInput) => {
-    // get coreInputs inputs
-    // need to know the selected input and it's state (i.e. turning existing on or off)
-    let existing = selectedInput.id === 'existing-conditions' ? true : false
+    let existing = selectedInput.value === 'existing-conditions' ? true : false
     let on = selectedInput.state
 
-    // check if existing or low stress is checked
     coreInputs.forEach(input => {
         if(existing) {
-            if(on) input.id === 'lowstress-islands' ? input.checked = false : input.checked = true
+            if(on) input.checked = true
             else input.checked = false
         }
 
         if(!existing && !on) {
-            if(input.id === 'existing-conditions') input.checked = false
+            if(input.value === 'existing-conditions') input.checked = false
         }
     })
 }
--- analysis/js/mapLayers.js
+++ analysis/js/mapLayers.js
@@ -22,40 +22,46 @@ const layers = {
             'line-color': '#f7f7f7'
         }
     },
-    // existingConditions: {
-    //     id: 'existing-conditions',
-    //     type: 'line',
-    //     source: 'lts',
-    //     'source-layer': 'existing_conditions_lts',
-    //     'paint': {
-    //         'line-width': 0.33,
-    //         'line-color': ['match',
-    //             ['get', 'lts_score'],
-    //             1, '#498434',
-    //             2, '#72bc58',
-    //             3, '#fcd842',
-    //             4, '#a50a0a',
-    //             '#fff'
-    //         ]
-    //     }
-    // },
-    // lowStressIslands: {
-    //     id: 'lowstress-islands',
-    //     type: 'line',
-    //     source: 'lts',
-    //     'source-layer': 'lowstress_islands',
-    //     'paint': {
-    //         'line-width': 0.33,
-    //         'line-color': ['match',
-    //             ['get', 'island_num'],
-    //             1, '#498434',
-    //             2, '#72bc58',
-    //             3, '#fcd842',
-    //             4, '#a50a0a',
-    //             '#fff'
-    //         ]
-    //     }
-    // }
+    existingConditions: {
+        id: 'existing-conditions',
+        type: 'line',
+        source: 'lts',
+        'source-layer': 'existing_conditions_lts',
+        'paint': {
+            'line-width': 0.33,
+            'line-color': ['match',
+                ['get', 'lts_score'],
+                1, '#498434',
+                2, '#72bc58',
+                3, '#fcd842',
+                4, '#a50a0a',
+                '#fff'
+            ]
+        }
+    },
+    lowStressIslands: {
+        id: 'lowstress-islands',
+        type: 'line',
+        source: 'lts',
+        'source-layer': 'lowstress_islands',
+        'paint': {
+            'line-width': 0.33,
+            'line-color': ['step',
+                ['get', 'island_num'],
+                '#fff',
+                18561, '#8dd3c7',
+                20055, '#ffffb3',
+                21549, '#bebada',
+                23043, '#fb8072',
+                24537, '#80b1d3',
+                26031, '#fdb462',
+                27525, '#b3de69',
+                29019, '#fccde5',
+                30513, '#d9d9d9',
+                32007, '#bc80bd'
+            ]
+        }
+    }
 }
 
 export default layers
