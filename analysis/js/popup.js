62928100a473c9d500448af4bc374af11d8aa94a
mmolta <mcmolta10@gmail.com>
2021-04-14 14:25:34 -0400

    fill out popup fields for lts layers

--- analysis/js/popup.js
+++ analysis/js/popup.js
@@ -10,13 +10,18 @@ const makePopupContent = (map, target, popup) => {
     .addTo(map)
 }
 
+// add slope 
 const makePopupHTML = props => {
     return `
         <span class="popup-span">
-            <strong>LTS Score:</strong> ${props.lts_score}<br />
+            <h3 class="popup-header">LTS Score: ${props.lts_score}</h3>
+            <p>LTS score is calculated as a function of the following 3 values:</p>
             <strong>Bike Facilities:</strong> ${props.bikefacili}<br />
+            <strong>Total Lanes:</strong> ${props.totnumlane}<br />
+            <strong>Speed:</strong> ${props.speed_lts} mph<br />
+            <hr />
             <strong>Segment Length:</strong> ${props.length} miles<br />
-            <strong>Total Lanes:</strong> ${props.totnumlane}
+            <strong>Slope:</strong> ${(props.slope_perc) * 100}%
         </span>
     `
 }
