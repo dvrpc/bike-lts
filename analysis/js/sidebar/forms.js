e3156f1f54d04e3757fdb4ced33f9d17bd1f08b9
mmolta <mcmolta10@gmail.com>
2021-05-21 13:11:12 -0400

    add select-content-replace class and set up content-replace obj

--- analysis/js/sidebar/forms.js
+++ analysis/js/sidebar/forms.js
@@ -20,6 +20,53 @@ const ltsFilters = {
 }
 const analysisLookup = ['priority', 'school', 'trails', 'transit', 'priority-ipd', 'school-ipd', 'trails-ipd', 'transit-ipd']
 
+const selectContentUpdates = {
+    ['regional-connectivity']: `
+        <fieldset class="sidebar-fieldset">
+            <legend>Levels of Traffic Stress (LTS)</legend>
+
+            <label class="sidebar-form-label">
+                <input type="checkbox" class="sidebar-form-checkbox core-lts" data-layer-type="filter" data-legend-type="lts" name="existing-conditions" value="lts-1" checked>
+                LTS 1
+            </label>
+            
+            <label class="sidebar-form-label">
+                <input type="checkbox" class="sidebar-form-checkbox core-lts" data-layer-type="filter" data-legend-type="lts" name="existing-conditions" value="lts-2" checked>
+                LTS 2
+            </label>
+
+            <label class="sidebar-form-label">
+                <input type="checkbox" class="sidebar-form-checkbox core-lts" data-layer-type="filter" data-legend-type="lts" name="existing-conditions" value="lts-3" checked>
+                LTS 3
+            </label>
+            
+            <label class="sidebar-form-label lts-layer-4">
+                <input type="checkbox" class="sidebar-form-checkbox core-lts" data-layer-type="filter" data-legend-type="lts" name="existing-conditions" value="lts-4" checked>
+                LTS 4
+            </label>
+        </fieldset>
+    `,
+    ['low-stress']: `
+        <fieldset class="sidebar-fieldset">
+            <legend>Low-Stress Areas</legend>
+                <label class="sidebar-form-label side-form-label-lts">
+                    <input type="checkbox" class="sidebar-form-checkbox" data-layer-type="toggle" data-legend-type="lowstress" name="lowstress-islands" value="lowstress-islands" checked>
+                    low-stress areas
+                </label>
+
+                <label class="sidebar-form-label">
+                    <input type="checkbox" class="sidebar-form-checkbox core-lts" data-layer-type="filter" data-legend-type="lts" name="existing-conditions" value="lts-3">
+                    LTS 3
+                </label>
+                
+                <label class="sidebar-form-label">
+                    <input type="checkbox" class="sidebar-form-checkbox core-lts" data-layer-type="filter" data-legend-type="lts" name="existing-conditions" value="lts-4">
+                    LTS 4
+                </label>
+        </fieldset>
+    `
+}
+
 const handleForms = (form, map) => {
     const formType = form.dataset.formType
 
