2f979a628f5d7dc44e44d3885e12d65043c1c8b7
mmolta <mcmolta10@gmail.com>
2021-08-05 11:40:58 -0400

    try out visual separation of lead-p and interactive content area

--- analysis/js/index.js
+++ analysis/js/index.js
@@ -18,8 +18,56 @@ const togglesContainer = sidebar.querySelector('#toggles-container')
 // @UPDATES TODO:
     // look into resetAnalysisLayers and resetLTSLayers fncs to see if they're still needed
 
-const demoAnalysis = `<h2>analysis jawn</h2>`
-const demoLTS = `<h2>LTS jawn</h2>`
+const demoLTS = `
+    <form autocomplete="off" class="sidebar-form" aria-label="core LTS layers form" data-form-type="content-replace">
+        <label class="sidebar-form-label">
+            <input type="checkbox" class="sidebar-form-checkbox core-lts" data-layer-type="filter" data-legend-type="lts" name="existing-conditions" value="lts-all" checked>
+            all LTS layers
+        </label>
+
+        <div class="sidebar-nested-inputs-wrapper flex-column">
+            <label class="sidebar-form-label">
+                <input type="checkbox" class="sidebar-form-checkbox core-lts" data-layer-type="filter" data-legend-type="lts" name="existing-conditions" value="lts-1" checked>
+                LTS 1
+            </label>
+            
+            <label class="sidebar-form-label">
+                <input type="checkbox" class="sidebar-form-checkbox core-lts" data-layer-type="filter" data-legend-type="lts" name="existing-conditions" value="lts-2" checked>
+                LTS 2
+            </label>
+
+            <label class="sidebar-form-label">
+                <input type="checkbox" class="sidebar-form-checkbox core-lts" data-layer-type="filter" data-legend-type="lts" name="existing-conditions" value="lts-3" checked>
+                LTS 3
+            </label>
+            
+            <label class="sidebar-form-label lts-layer-4">
+                <input type="checkbox" class="sidebar-form-checkbox core-lts" data-layer-type="filter" data-legend-type="lts" name="existing-conditions" value="lts-4" checked>
+                LTS 4
+            </label>
+        </div>
+    </form>
+
+    <hr class="sidebar-hr" />
+
+    <form autocomplete="off" class="sidebar-form" aria-label="core LTS layers form" data-form-type="content-replace">
+        <div class="flex-column">
+            <label class="sidebar-form-label side-form-label-lts">
+                <input type="checkbox" class="sidebar-form-checkbox" data-layer-type="toggle" data-legend-type="lowstress" name="lowstress-islands" value="lowstress-islands">
+                low-stress areas
+            </label>
+            
+        <hr class="sidebar-hr" />
+
+            <label class="sidebar-form-label">
+                <input type="checkbox" class="sidebar-form-checkbox" data-layer-type="toggle" data-legend-type="facilities" name="layer" value="facilities">
+                bicycle facilities
+            </label>
+        </div>
+    </form>
+`
+
+const demoAnalysis = `<h2>Analysis jawn</h2>`
 
 const togglesContent = {
     'lts-tab': demoLTS,
