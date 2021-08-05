fe12bdadea4ab6e22172750d0102c0beb54c4504
mmolta <mcmolta10@gmail.com>
2021-08-05 11:06:30 -0400

    initial set up of tab click functionality

--- analysis/js/index.js
+++ analysis/js/index.js
@@ -4,14 +4,39 @@ import layers from './map/mapLayers.js'
 import mapUtils from './map/mapUtils.js'
 import handleForms from './sidebar/forms.js'
 import { makePopup, makePopupContent } from './map/popup.js'
-import { resetLTSLayers, resetAnalysisLayers } from './sidebar/formsUtils.js'
+// import { resetLTSLayers, resetAnalysisLayers } from './sidebar/formsUtils.js'
 // import createFeedbackForm from './sidebar/feedback.js'
 
 const sidebar = document.getElementById('sidebar')
 const forms = sidebar.querySelectorAll('.sidebar-form')
+const tabs = Array.from(sidebar.querySelector('#sidebar-tabs').children)
+const togglesContainer = sidebar.querySelector('#toggles-container')
 // const feedbackBtn = sidebar.querySelector('#feedback-btn')
-const resetAnalysisBtn = sidebar.querySelector('#clear-analysis-btn')
-const resetLTSBtn = sidebar.querySelector('#clear-lts-btn')
+// const resetAnalysisBtn = sidebar.querySelector('#clear-analysis-btn')
+// const resetLTSBtn = sidebar.querySelector('#clear-lts-btn')
+
+// @UPDATES TODO:
+    // look into resetAnalysisLayers and resetLTSLayers fncs to see if they're still needed
+
+// sidebar
+tabs.forEach(tab => {
+    tab.onclick = e => {
+        if(tab.classList.contains('sidebar-tab-active')) return false
+        const tabID = tab.id
+
+        const currentActive = tab.previousElementSibling || tab.nextElementSibling
+
+        currentActive.classList.remove('sidebar-tab-active')
+        currentActive.classList.add('sidebar-tab-inactive')
+        
+        tab.classList.remove('sidebar-tab-inactive')
+        tab.classList.add('sidebar-tab-active')
+
+        // use tabID to update togglesContainer content
+
+        // use tabID to update map layers
+    }
+})
 
 // map
 const map = makeMap()
@@ -24,8 +49,8 @@ map.on('load', () => {
     for(const layer in layers) map.addLayer(layers[layer], firstSymbolId)
 
     forms.forEach(form => handleForms(form, map))
-    resetAnalysisBtn.onclick = () => resetAnalysisLayers(map)
-    resetLTSBtn.onclick = e => resetLTSLayers(map, e)
+    // resetAnalysisBtn.onclick = () => resetAnalysisLayers(map)
+    // resetLTSBtn.onclick = e => resetLTSLayers(map, e)
 
     map.on('click', 'existing-conditions', e => makePopupContent(map, e, ltsLayersPopup))
     map.on('mousemove', 'existing-conditions', () => map.getCanvas().style.cursor = 'pointer')
