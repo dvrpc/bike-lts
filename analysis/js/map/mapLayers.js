f7f1bfbe39721bea0583da7903a67a1bb1fb50f6
Marc Molta <mcmolta10@gmail.com>
2021-09-09 13:57:11 -0400

    reincorporate lts layers

--- analysis/js/map/mapLayers.js
+++ analysis/js/map/mapLayers.js
@@ -22,51 +22,51 @@ const layers = {
             'line-color': '#242424'
         }
     },
-    // existingConditions: {
-    //     id: 'existing-conditions',
-    //     type: 'line',
-    //     source: 'lts',
-    //     'source-layer': 'existing_conditions_lts',
-    //     'paint': {
-    //         'line-width': ['interpolate', 
-    //             ['linear'], ['zoom'],
-    //             8.35, 0.33,
-    //             10, 0.99,
-    //             11, 2.5,
-    //             17, 3,
-    //             20, 4
-    //         ],
-    //         'line-color': ['match',
-    //             ['get', 'lts_score'],
-    //             1, '#498434',
-    //             2, '#72bc58',
-    //             3, '#fcd842',
-    //             4, '#a50a0a',
-    //             '#fff'
-    //         ]
-    //     }
-    // },
-    // 'lowstress-click': {
-    //     id: 'lowstress-click',
-    //     type: 'line',
-    //     source: 'lts',
-    //     'source-layer': 'lowstress_islands',
-    //     paint: {
-    //         'line-width': ['interpolate', 
-    //             ['linear'], ['zoom'],
-    //             8.35, 1.2,
-    //             10, 1.8,
-    //             11, 3,
-    //             17, 5,
-    //             20, 7
-    //         ],
-    //         'line-color': '#fff',
-    //         'line-dasharray': [2, 0.5]
-    //     },
-    //     filter: ['==',
-    //         'island_num', 0
-    //     ]
-    // }
+    existingConditions: {
+        id: 'existing-conditions',
+        type: 'line',
+        source: 'lts',
+        'source-layer': 'existing_conditions_lts',
+        'paint': {
+            'line-width': ['interpolate', 
+                ['linear'], ['zoom'],
+                8.35, 0.33,
+                10, 0.99,
+                11, 2.5,
+                17, 3,
+                20, 4
+            ],
+            'line-color': ['match',
+                ['get', 'lts_score'],
+                1, '#498434',
+                2, '#72bc58',
+                3, '#fcd842',
+                4, '#a50a0a',
+                '#fff'
+            ]
+        }
+    },
+    'lowstress-click': {
+        id: 'lowstress-click',
+        type: 'line',
+        source: 'lts',
+        'source-layer': 'lowstress_islands',
+        paint: {
+            'line-width': ['interpolate', 
+                ['linear'], ['zoom'],
+                8.35, 1.2,
+                10, 1.8,
+                11, 3,
+                17, 5,
+                20, 7
+            ],
+            'line-color': '#fff',
+            'line-dasharray': [2, 0.5]
+        },
+        filter: ['==',
+            'island_num', 0
+        ]
+    }
 }
 
 export default layers
