7527dfa3f905ff6cbdb4e28bbe85b7ae60c6e738
mmolta <mcmolta10@gmail.com>
2021-09-23 17:42:47 -0400

    begin setting up intersection observer for nav translate

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
 import sceneObjs from './scenes.js'
+import { observeNav } from './observers.js';
+
+const topNav = document.getElementById('top-nav')
 
 // init controller & add scenes
 const controller = new ScrollMagic.Controller();
 sceneObjs.forEach(scene => scene.addTo(controller))
+
+// observe top nav
+const options = {
+    rootMargin: '0px',
+    threshold: 0.1
+}
+const observer = new IntersectionObserver(observeNav, options)
+
+observer.observe(document.getElementById('scroll-story-main'))
---
+++ js/observers.js
@@ -0,0 +1,10 @@
+const observeNav = (entries, observer) => {
+    // entries.forEach(entry => {
+    //     if(entry.intersectionRatio > 0) entry.target.style.transform = 'translateY(-100%)'
+    //     else entry.target.style.display = 'translateY(0)'
+    // })
+    console.log('entris are ', entries)
+    console.log('observer is ', observer)
+}
+
+export { observeNav }
