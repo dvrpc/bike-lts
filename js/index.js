eddb18be67625a88d5db5ea99337421ea96a2266
mmolta <mcmolta10@gmail.com>
2021-09-24 09:57:25 -0400

    update side-nav sticky calc to account for top-nav

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import sceneObjs from './scenes.js'
 
 const topNav = document.querySelector('.top-nav');
+const sideNav = document.getElementById('scroll-story-nav')
 
 // init controller & add scenes
 const controller = new ScrollMagic.Controller();
@@ -16,7 +17,11 @@ const callback = entries => {
     }
   })
 }
-
 const observer = new IntersectionObserver(callback);
-
 observer.observe(document.getElementById('story-intro'))
+
+// calculate side-nav sticky
+const sideNavHeight = sideNav.offsetHeight
+const windowHeightAdjusted = window.innerHeight + topNav.offsetHeight
+const diff = (windowHeightAdjusted - sideNavHeight) / 2
+sideNav.style.top = diff + 'px'
