22ed6ef9b685232b972a4ba34782fec43a4d567b
mmolta <mcmolta10@gmail.com>
2021-09-24 09:15:15 -0400

    settle on nav intersection solution

--- js/index.js
+++ js/index.js
@@ -1,22 +1,23 @@
 import sceneObjs from './scenes.js'
-import { observeNav } from './observers.js';
+
+const topNav = document.querySelector('.top-nav');
 
 // init controller & add scenes
 const controller = new ScrollMagic.Controller();
 sceneObjs.forEach(scene => scene.addTo(controller))
 
-const intro = document.getElementById('story-intro')
-
-const topNav = document.querySelector('.top-nav');
-
-const observer = new IntersectionObserver(entries => {
+// @TODO: proper way to do this w/entry.target instead of topNav variable
+  // target of intro and root of top-nav doesn't work that way I'd expect..
+const callback = entries => {
   entries.forEach(entry => {
     if (entry.intersectionRatio > 0) {
-        topNav.classList.remove('top-nav-visible')
+      topNav.classList.remove('top-nav-visible')
     } else {
-        topNav.classList.add('top-nav-visible')
+      topNav.classList.add('top-nav-visible')
     }
-  });
-});
+  })
+}
 
-observer.observe(intro)
+const observer = new IntersectionObserver(callback);
+
+observer.observe(document.getElementById('story-intro'))
