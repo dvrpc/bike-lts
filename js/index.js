e7c845f1a6816a778968eb15296b8f85dd8002bb
mmolta <mcmolta10@gmail.com>
2021-09-23 23:36:49 -0400

    initial setup of intersection observer nav reveal

--- js/index.js
+++ js/index.js
@@ -1,17 +1,22 @@
 import sceneObjs from './scenes.js'
 import { observeNav } from './observers.js';
 
-const topNav = document.getElementById('top-nav')
-
 // init controller & add scenes
 const controller = new ScrollMagic.Controller();
 sceneObjs.forEach(scene => scene.addTo(controller))
 
-// observe top nav
-const options = {
-    rootMargin: '0px',
-    threshold: 0.1
-}
-const observer = new IntersectionObserver(observeNav, options)
+const intro = document.getElementById('story-intro')
 
-observer.observe(document.getElementById('scroll-story-main'))
+const topNav = document.querySelector('.top-nav');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+        topNav.classList.remove('top-nav-visible')
+    } else {
+        topNav.classList.add('top-nav-visible')
+    }
+  });
+});
+
+observer.observe(intro)
--- js/observers.js
+++ js/observers.js
@@ -1,10 +1,11 @@
 const observeNav = (entries, observer) => {
-    // entries.forEach(entry => {
-    //     if(entry.intersectionRatio > 0) entry.target.classList.remove('top-nav-visible')
-    //     else entry.target.classList.add('top-nav-visible')
-    // })
-    console.log('entris are ', entries)
-    console.log('observer is ', observer)
+    entries.forEach(entry => {
+        console.log('entry ', entry)
+        // if(entry.intersectionRatio > 0) entry.target.classList.remove('top-nav-visible')
+        // else entry.target.classList.add('top-nav-visible')
+    })
+    // console.log('entris are ', entries)
+    // console.log('observer is ', observer)
 }
 
 export { observeNav }
